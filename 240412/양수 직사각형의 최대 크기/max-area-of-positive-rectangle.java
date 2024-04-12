aa98ee893be771ad838a1aae412b33d869fbe391
Im WonYong <38348704+ImWonYong@users.noreply.github.com>
2024-04-12 17:31:05 +0900

    Title: 양수 직사각형의 최대 크기, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240412/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int m;
+    public static int[][] board;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        board = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        int maxSquare = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = i; k < n; k++) {
+                    for (int l = j; l < n; l++) {
+                        if (isPositiveSquare(i, j, k, l))
+                            maxSquare = Math.max(maxSquare, getSize(i, j, k, l)); 
+                    }
+                }
+            }
+        }
+
+        System.out.println(maxSquare == 0 ? -1 : maxSquare);
+    }
+
+    public static boolean isPositiveSquare(int x1, int y1, int x2, int y2) {
+        for (int i = x1; i <= x2; i++) {
+            for (int j = y1; j <= y2; j++) {
+                if (board[i][j] <= 0) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int getSize(int x1, int y1, int x2, int y2) {
+        return (x2 - x1 + 1) * (y2 - y1 + 1);
+    }
+}
