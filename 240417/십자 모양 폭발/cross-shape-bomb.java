67b5c7c8780da4da047f9cf7d43d5f9fed3948e5
Im WonYong <38348704+ImWonYong@users.noreply.github.com>
2024-04-17 10:36:43 +0900

    Title: 십자 모양 폭발, Time: 145ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240417/십자 모양 폭발/cross-shape-bomb.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Main {
+    public static int n;
+    public static int[][] board;
+    public static boolean[][] checked;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        board = new int[n][n];
+        checked = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        int r = sc.nextInt() - 1;
+        int c = sc.nextInt() - 1;
+
+        checkExplode(r, c);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (checked[i][j])
+                    removeBlock(i, j);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void removeBlock(int r, int c) {
+        
+        int[] temp = new int[n];
+        int tempIndex = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (i != r)
+                temp[tempIndex--] = board[i][c];
+        }
+
+        for (int i = 0; i < n; i++) {
+            board[i][c] = temp[i];
+        }
+    }
+
+    public static void checkExplode(int r, int c) {
+        int length = board[r][c] - 1;
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        checked[r][c] = true;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < length; j++) {
+                int nr = r + dx[i];
+                int nc = c + dy[i];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n)
+                    checked[nr][nc] = true;
+            }
+        }
+    }
+}
