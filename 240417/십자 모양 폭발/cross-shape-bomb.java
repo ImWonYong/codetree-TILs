3f9f9130d6297feee4e318f32a2cbaf18600579f
Im WonYong <38348704+ImWonYong@users.noreply.github.com>
2024-04-17 10:52:43 +0900

    Title: 십자 모양 폭발, Time: 948ms, Memory: 34MB, Status: Passed - CodeTree

--- 240417/십자 모양 폭발/cross-shape-bomb.java
+++ 240417/십자 모양 폭발/cross-shape-bomb.java
@@ -3,7 +3,36 @@ import java.util.*;
 public class Main {
     public static int n;
     public static int[][] board;
-    public static boolean[][] checked;
+    public static int[][] tempBoard;
+
+    public static boolean isBombRange(int x, int y, int r, int c, int bombRange) {
+        return (x == r || y == c) && Math.abs(x - r) + Math.abs(y - c) < bombRange;
+    }
+
+    public static void bomb(int r, int c) {
+        int bombRange = board[r][c];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isBombRange(i, j, r, c, bombRange))
+                    board[i][j] = 0;
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            int nextRow = n - 1;
+            for (int i = n - 1; i >= 0; i--) {
+                if (board[i][j] > 0)
+                    tempBoard[nextRow--][j] = board[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = tempBoard[i][j];
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,7 +40,7 @@ public class Main {
         n = sc.nextInt();
 
         board = new int[n][n];
-        checked = new boolean[n][n];
+        tempBoard = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -22,57 +51,13 @@ public class Main {
         int r = sc.nextInt() - 1;
         int c = sc.nextInt() - 1;
 
-        checkExplode(r, c);     
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (checked[i][j])
-                    removeBlock(i, j);
-            }
-        }
+        bomb(r, c);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
-        }
-    }
-
-    public static void removeBlock(int r, int c) {
-        
-        int[] temp = new int[n];
-        int tempIndex = n - 1;
-
-        for (int i = n - 1; i >= 0; i--) {
-            if (i != r)
-                temp[tempIndex--] = board[i][c];
-        }
-
-        for (int i = 0; i < n; i++) {
-            board[i][c] = temp[i];
-        }
-    }
-
-    public static void checkExplode(int r, int c) {
-        int length = board[r][c] - 1;
-
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        checked[r][c] = true;
-        for (int i = 0; i < 4; i++) {
-            int tempR = r;
-            int tempC = c;
-            for (int j = 0; j < length; j++) {
-                int nr = tempR + dx[i];
-                int nc = tempC + dy[i];
-                if (nr >= 0 && nr < n && nc >= 0 && nc < n) {
-                    checked[nr][nc] = true;
-                    tempR = nr;
-                    tempC = nc;
-                }
-            }
         }
     }
 }
