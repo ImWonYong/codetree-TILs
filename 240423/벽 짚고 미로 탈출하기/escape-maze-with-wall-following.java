f6db6f7bd7c43bfc59714c6b294456761de4cd72
Im WonYong <38348704+ImWonYong@users.noreply.github.com>
2024-04-23 21:22:23 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 175ms, Memory: 11MB, Status: Passed - CodeTree

--- 240423/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
+++ 240423/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -1,97 +1,117 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 100;
+    public static final int DIR_NUM = 4;
+    
+    // 전역 변수 선언:
     public static int n;
-    public static int x;
-    public static int y;
-    public static char[][] board;
-    public static boolean[][][] checked;
-    public static int dir;
-    public static int escapeTime;
-
-    public static int[] dx = {0, -1, 0, 1};
-    public static int[] dy = {1, 0, -1, 0};
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        n = sc.nextInt();
-        x = sc.nextInt() - 1;
-        y = sc.nextInt() - 1;
-
-        board = new char[n][n];
-        checked = new boolean[n][n][4];
-        dir = 0;
-
-        for (int i = 0; i < n; i++) {
-            board[i] = sc.next().toCharArray();
-        }
-
-        do {
-            simulate();
-        } while (isRange(x, y));
-
-        System.out.println(escapeTime);
+    
+    public static int currX, currY, currDir; // 현재 위치와 방향을 의미합니다.
+    public static char[][] a = new char[MAX_N + 1][MAX_N + 1];
+    
+    // 미로 탈출이 불가능한지 여부를 판단하기 위해
+    // 동일한 위치에 동일한 방향으로 진행했던 적이 있는지를
+    // 표시해주는 배열입니다.
+    public static boolean[][][] visited = new boolean[MAX_N + 1][MAX_N + 1][DIR_NUM];
+    
+    public static int elapsedTime;
+    
+    // 범위가 격자 안에 들어가는지 확인합니다.
+    public static boolean inRange(int x, int y) {
+        return 1 <= x && x <= n && 1 <= y && y <= n;
     }
-
+    
+    // 해당 위치에 벽이 있으면 이동이 불가합니다.
+    public static boolean wallExist(int x, int y) {
+        return inRange(x, y) && a[x][y] == '#';
+    }
+    
+    // 조건에 맞춰 움직여봅니다.
     public static void simulate() {
-
-        if (checked[x][y][dir]) {
-            System.out.println(-1);
+        // 현재 위치에 같은 방향으로 진행한 적이 이미 있었는지 확인합니다.
+        // 이미 한 번 겪었던 상황이라면, 탈출이 불가능 하다는 의미이므로 
+        // -1을 출력하고 프로그램을 종료합니다.
+        if(visited[currX][currY][currDir]) {
+            System.out.print(-1);
             System.exit(0);
         }
-
-        checked[x][y][dir] = true;
-
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
-
-        if (wallExist(nx, ny))
-            dir = (dir - 1 + 4) % 4;
-
-        else if (!isRange(nx, ny)) {
-            x = nx;
-            y = ny;
-            escapeTime++;
-        } else {
-            int rx = nx + dx[(dir + 1) % 4];
-            int ry = ny + dy[(dir + 1) % 4];
-
-            if (wallExist(rx, ry)) {
-                x = nx;
-                y = ny;
-                escapeTime++;
-            } else {
-                x = rx;
-                x = ry;
-                dir = (dir + 1) % 4;
-                escapeTime += 2;
+        // 현재 상황이 다시 반복되는지를 나중에 확인하기 위해
+        // 현재 상황에 해당하는 곳에 visited 값을 true로 설정합니다.
+        visited[currX][currY][currDir] = true;
+    
+        int[] dx = new int[]{0, 1, 0, -1};
+        int[] dy = new int[]{1, 0, -1, 0};
+    
+        int nextX = currX + dx[currDir];
+        int nextY = currY + dy[currDir];
+    
+        // Step1
+        
+        // 바라보고 있는 방향으로 이동하는 것이 불가능한 경우에는
+        // 반 시계 방향으로 90' 방향을 바꿉니다.
+        if(wallExist(nextX, nextY))
+            currDir = (currDir - 1 + 4) % 4;
+        
+        // Step2
+        
+        // Case1
+        // 바라보고 있는 방향으로 이동하는 것이 가능한 경우 중
+        // 바로 앞이 격자 밖이라면 탈출합니다.
+        else if(!inRange(nextX, nextY)) {
+            currX = nextX; currY = nextY;
+            elapsedTime++;
+        }
+    
+        // Case2 & Case3
+        // 바로 앞이 격자 안에서 이동할 수 있는 곳이라면 
+        else {
+            // 그 방향으로 이동했다 가정했을 때 바로 오른쪽에 짚을 벽이 있는지 봅니다.
+            int rx = nextX + dx[(currDir + 1) % 4];
+            int ry = nextY + dy[(currDir + 1) % 4];
+    
+            // Case2
+            // 그대로 이동해도 바로 오른쪽에 짚을 벽이 있다면 
+            // 해당 방향으로 한 칸 이동합니다.
+            if(wallExist(rx, ry)) {
+                currX = nextX; currY = nextY;
+                elapsedTime++;
+            }
+    
+            // Case3
+            // 그렇지 않다면 2칸 이동후 방향을 시계방향으로 90' 방향을 바꿉니다.
+            else {
+                currX = rx; currY = ry;
+                currDir = (currDir + 1) % 4;
+                elapsedTime += 2;
             }
         }
     }
 
-    public static boolean wallExist(int x, int y) {
-        return isRange(x, y) && board[x][y] == '#';
-    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        currX = sc.nextInt();
+        currY = sc.nextInt();
+        
+        // 처음에는 우측 방향을 바라보고 시작합니다.
+        currDir = 0;
 
-    public static boolean isRange(int x, int y) {
-        return x >= 0 && x < n && y >= 0 && y < n;
+        for(int i = 1; i <= n; i++) {
+            String str = sc.next();
+            for(int j = 1; j <= n; j++)
+                a[i][j] = str.charAt(j - 1);
+        }
+        
+        do {
+            // 조건에 맞춰 움직여봅니다.
+            simulate();
+        
+        // 격자를 빠져나오기 전까지 계속 반복합니다.
+        } while(inRange(currX, currY)); 
+
+        // 출력:
+        System.out.print(elapsedTime);
     }
 }
-
-/*
-
-    오른쪽 벽을 짚고 쭉 따라가는 방식.
-    격자 밖을 벗어났을 때 미로를 탈출
-
-    1. 바라보고 있는 방향으로 이동하는 것이 가능하지 않으면 반 시계 방향 90도 회전
-    2. 바라보고 있는 방향으로 이동 가능한 경우
-        - 격자 밖이면 탈출
-        - 이동했을 때를 기준으로 짚을 벽이 있으면 그 방향으로 한 칸 이동
-        - 오른쪽에 벽이 존재하지 않으면, 현재 방향으로 한칸 이동 후 시계 방향으로 90도 틀어 한칸 전진하여 오른쪽 벽이 있게 함
-
-    
-    언제 영원히 돌게될까??
-    1. 모든 사방이 벽으로 둘러쌓여있을 때
-    2. 같은 곳을 뱅글뱅글 돌 때
-*/
