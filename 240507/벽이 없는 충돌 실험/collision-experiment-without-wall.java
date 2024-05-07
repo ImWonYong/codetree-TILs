cc3dc2a5f3770caf7419d1e3b85dcd7ab6543655
Im WonYong <38348704+ImWonYong@users.noreply.github.com>
2024-05-07 10:47:00 +0900

    Title: 벽이 없는 충돌 실험, Time: 2783ms, Memory: 184MB, Status: Passed - CodeTree

--- 240507/벽이 없는 충돌 실험/collision-experiment-without-wall.java
+++ 240507/벽이 없는 충돌 실험/collision-experiment-without-wall.java
@@ -3,8 +3,7 @@ import java.util.ArrayList;
 
 class Marble {
     int x, y, weight, dir, num;
-
-    public Marble(int x, int y, int wieght, int dir, int num) {
+    public Marble(int x, int y, int weight, int dir, int num) {
         this.x = x;
         this.y = y;
         this.weight = weight;
@@ -19,17 +18,17 @@ public class Main {
     public static final int COORD_SIZE = 4000;
     public static final int OFFSET = 2000;
     public static final int DIR_NUM = 4;
-
+    
     public static int t, n;
-
+    
     public static int[] mapper = new int[ASCII_NUM];
-
+    
     public static int[] dx = new int[]{0, 1, -1, 0};
     public static int[] dy = new int[]{1, 0, 0, -1};
-
+    
     public static int currTime;
     public static int lastCollisionTime;
-
+    
     public static ArrayList<Marble> marbles = new ArrayList<>();
     public static ArrayList<Marble> nextMarbles = new ArrayList<>();
     public static int[][] nextMarbleIndex = new int[COORD_SIZE + 1][COORD_SIZE + 1];
@@ -40,7 +39,7 @@ public class Main {
         int weight = marble.weight;
         int dir = marble.dir;
         int num = marble.num;
-
+        
         int nx = x + dx[dir], ny = y + dy[dir];
         return new Marble(nx, ny, weight, dir, num);
     }
@@ -51,7 +50,7 @@ public class Main {
     
         return nextMarbleIndex[targetX][targetY];
     }
-
+    
     public static Marble Collide(Marble marble1, Marble marble2) {
         int weight1 = marble1.weight;
         int num1 = marble1.num;
@@ -71,36 +70,38 @@ public class Main {
     
         return x < 0 || x > COORD_SIZE || y < 0 || y > COORD_SIZE;
     }
-
+    
     public static void pushNextMarble(Marble marble) {
-        if (outOfActiveCoordinate(marble))
+        if(outOfActiveCoordinate(marble))
             return;
-
+    
         int index = findDuplicateMarble(marble);
-
-        if (index == BLANK) {
+    
+        if(index == BLANK) {
             nextMarbles.add(marble);
 
             int x = marble.x;
             int y = marble.y;
             nextMarbleIndex[x][y] = (int) (nextMarbles.size()) - 1;
-        } else {
+        }
+    
+        else {
             Marble newMarble = Collide(nextMarbles.get(index), marble);
             nextMarbles.set(index, newMarble);
             lastCollisionTime = currTime;
         }
     }
-
+    
     public static void simulate() {
-        for (int i = 0; i < (int) marbles.size(); i++) {
+        for(int i = 0; i < (int) marbles.size(); i++) {
             Marble nextMarble = Move(marbles.get(i));
-
+    
             pushNextMarble(nextMarble);
         }
-
+        
         marbles = (ArrayList<Marble>) nextMarbles.clone();
 
-        for (int i = 0; i < (int) nextMarbles.size(); i++) {
+        for(int i = 0; i < (int) nextMarbles.size(); i++) {
             int x = nextMarbles.get(i).x;
             int y = nextMarbles.get(i).y;
             nextMarbleIndex[x][y] = BLANK;
@@ -117,34 +118,32 @@ public class Main {
 
         t = sc.nextInt();
 
-        for (int i = 0; i <= COORD_SIZE; i++) {
-            for (int j = 0; j <= COORD_SIZE; j++) {
+        for(int i = 0; i <= COORD_SIZE; i++)
+             for(int j = 0; j <= COORD_SIZE; j++)
                 nextMarbleIndex[i][j] = BLANK;
-            }
-        }
 
-        while (t-- > 0) {
-            marbles = new ArrayList();
+        while(t-- > 0) {
+            marbles = new ArrayList<>();
             lastCollisionTime = -1;
 
+            // 입력:
             n = sc.nextInt();
-
-            for (int i = 1; i <= n; i++) {
+            for(int i = 1; i <= n; i++) {
                 int x = sc.nextInt();
                 int y = sc.nextInt();
                 int weight = sc.nextInt();
-                char d = sc.next().charAt(0);                
-            
+                char d = sc.next().charAt(0);
+
                 x *= 2; y *= 2;
                 x += OFFSET; y += OFFSET;
                 marbles.add(new Marble(x, y, weight, mapper[d], i));
             }
 
-            for (int i = 1; i <= COORD_SIZE; i++) {
+            for(int i = 1; i <= COORD_SIZE; i++) {
                 currTime = i;
                 simulate();
             }
-
+            
             System.out.println(lastCollisionTime);
         }
     }
